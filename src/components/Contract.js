bd553d567c7cebffbadbaacad72f74239ccb3fff
sharmila96-wt <52949147+sharmila96-wt@users.noreply.github.com>
2021-09-19 23:16:08 +0530

    Update Contract.js

--- src/components/Contract.js
+++ src/components/Contract.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import * as nearAPI from 'near-api-js';
-import { handleMint } from '../state/actions';
+import { handleSale } from '../state/actions';
 import { 
 	isAccountTaken,
 	networkId,
@@ -15,42 +15,32 @@ export const Contract = ({ near, update, account }) => {
 
 	const [media, setMedia] = useState('');
 	const [validMedia, setValidMedia] = useState('');
-	const [royalties, setRoyalties] = useState({});
-	const [royalty, setRoyalty] = useState([]);
 	const [receiver, setReceiver] = useState([]);
-
+        const [price, setPrice] = useState([]);
 	return <>
-		<h4>Mint Something</h4>
+		<h4>Upload image link to sell</h4>
 		<input className="full-width" placeholder="Image Link" value={media} onChange={(e) => setMedia(e.target.value)} />
 		<img src={media} onLoad={() => setValidMedia(true)} onError={() => setValidMedia(false)} />
 		
-		{ !validMedia && <p>Image link is invalid.</p> }
+		{ !validMedia && <p>(Image link is invalid)</p> }
 		
-		<h4>Royalties</h4>
-		{
-			Object.keys(royalties).length > 0 ? 
-				Object.entries(royalties).map(([receiver, royalty]) => <div key={receiver}>
-					{receiver} - {royalty} % <button onClick={() => {
-						delete royalties[receiver];
-						setRoyalties(Object.assign({}, royalties));
-					}}>❌</button>
-				</div>)
-				:
-				<p>No royalties added yet.</p>
-		}
+		<h4>Account ID</h4>
+		
 		<input className="full-width" placeholder="Account ID" value={receiver} onChange={(e) => setReceiver(e.target.value)} />
-		<input type="number" className="full-width" placeholder="Percentage" value={royalty} onChange={(e) => setRoyalty(e.target.value)} />
-		<button onClick={async () => {
+
+				<button onClick={async () => {
 			const exists = await isAccountTaken(receiver);
+			handleSale(account, media, validMedia);
 			if (!exists) return alert(`Account: ${receiver} does not exist on ${networkId ==='default' ? 'testnet' : 'mainnet'}.`);
-			setRoyalties(Object.assign({}, royalties, {
-				[receiver]: royalty
-			}));
-		}}>Add Royalty</button>
+			
+			
+		}}>upload</button>
+		{
+			}
 
-		<div className="line"></div>
 
-		<button onClick={() => handleMint(account, royalties, media, validMedia)}>Mint</button>
+		
+		
+	
 	</>;
 };
-
